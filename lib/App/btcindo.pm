59cbfed1897c32fae4dc57f8c1fe894bcc05500b
perlancar (on pc-office) <perlancar@gmail.com>
2017-05-31 19:35:28 +0700

    Initial commit

---
+++ lib/App/btcindo.pm
@@ -0,0 +1,16 @@
+package App::btcindo;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI for bitcoin.co.id
+
+=head1 SYNOPSIS
+
+Please see included script L<btcindo>.
+
+
+=head1 SEE ALSO
+
+L<Finance::BTCInfo>
